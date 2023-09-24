fa297ce586d3bd2ca1c462e8c8edcbd5c0d61704
HANLeeeee <hanleeeee@naver.com>
2023-09-25 06:39:30 +0900

    rename: constant 파일명 오타 수정

---
+++ Pico/Common/CommonButton.swift
@@ -0,0 +1,20 @@
+//
+//  CommonButton.swift
+//  Pico
+//
+//  Created by 최하늘 on 2023/09/25.
+//
+
+import UIKit
+
+final class CommonButton: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
