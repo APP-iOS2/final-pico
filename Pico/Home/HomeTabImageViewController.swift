308cabb8234dbc24fc90b2ac7afc556e7e596149
DAEJINLIM <daejin.limm@gmail.com>
2023-09-25 21:22:07 +0900

    Feat: 버튼, 라벨 추가

--- Pico/Home/HomeTabImageViewController.swift
+++ Pico/Home/HomeTabImageViewController.swift
@@ -6,11 +6,242 @@
 //
 
 import UIKit
+import SnapKit
 
 class HomeTabImageViewController: UIViewController {
-
+    private let paddingVertical = 25
+    private let paddingBottom = 30
+    private let buttonFrame = 65
+    
+    private let imageUrl: [String] = [
+        "https://image5jvqbd.fmkorea.com/files/attach/new2/20211225/3655109/3113058505/4195166827/e130faca7194985e4f162b3583d52853.jpg",
+        "https://img.dmitory.com/img/202107/2lh/a8H/2lha8HnRr6Q046GGGQ0uwM.jpg",
+        "https://i.namu.wiki/i/jUHcYJjORbNSurOw8cwl-g8jduAT01mhJJkF5oDbvyae_1hkSExnUQ0I5fDKgebUKzSFjSFhRheeSI9-rfpuDU8RJ9wqo5KwIodMVjuzKT2o6RK0IutUtsKWZrYxzT-cOvxKhbPm9c3PXo5H-OvBCA.webp"
+    ]
+    //    private var imageUrl: [String]
+    //    init(imageUrl: [String]) {
+    //            self.imageUrl = imageUrl
+    //            super.init(nibName: nil, bundle: nil)
+    //        }
+    //
+    //    required init?(coder: NSCoder) {
+    //        fatalError("init(coder:) has not been implemented")
+    //    }
+    
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.isPagingEnabled = true
+        return scrollView
+    }()
+    
+    private let pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.currentPageIndicatorTintColor = UIColor.black
+        pageControl.pageIndicatorTintColor = UIColor.lightGray
+        return pageControl
+    }()
+    
+    private let pickBackButton: UIButton = {
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 40, weight: .regular)
+        let image = UIImage(systemName: "arrow.uturn.backward", withConfiguration: imageConfig)
+        let button = UIButton()
+        button.setImage(image, for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = .gray.withAlphaComponent(0.5)
+        button.frame = CGRect(x: 0, y: 0, width: 65, height: 65)
+        button.layer.cornerRadius = 0.5 * button.bounds.size.width
+        return button
+    }()
+    
+    private let disLikeButton: UIButton = {
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 40, weight: .regular)
+        let image = UIImage(systemName: "hand.thumbsdown.fill", withConfiguration: imageConfig)
+        let button = UIButton()
+        button.setImage(image, for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = .picoBlue
+        button.frame = CGRect(x: 0, y: 0, width: 65, height: 65)
+        button.layer.cornerRadius = 0.5 * button.bounds.size.width
+        return button
+    }()
+    
+    private let likeButton: UIButton = {
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 40, weight: .regular)
+        let image = UIImage(systemName: "hand.thumbsup.fill", withConfiguration: imageConfig)
+        let button = UIButton()
+        button.setImage(image, for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = .picoBlue
+        button.frame = CGRect(x: 0, y: 0, width: 65, height: 65)
+        button.layer.cornerRadius = 0.5 * button.bounds.size.width
+        return button
+    }()
+    
+    private let infoHStack: UIStackView = {
+        let infoHStack = UIStackView()
+        infoHStack.axis = .horizontal
+        infoHStack.alignment = .top
+//        infoHStack.backgroundColor = .gray.withAlphaComponent(0.5)
+        return infoHStack
+    }()
+    
+    private let infoVStack: UIStackView = {
+        let infoVStack = UIStackView()
+        infoVStack.axis = .vertical
+        infoVStack.spacing = 5
+        return infoVStack
+    }()
+    
+    private let infoNameAgeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "에스파윈터, 25"
+        label.font = .picoTitleFont
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 25, weight: .bold)
+        return label
+    }()
+    
+    private let infoLocationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "서울특별시 강남구, 12.5km"
+        label.font = .picoTitleFont
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        return label
+    }()
+    
+    private let infoMBTILabel: UILabel = {
+        let label = UILabel()
+        label.text = "ENTP"
+        label.textColor = .white
+        label.frame = CGRect(x: 0, y: 0, width: 45, height: 25)
+//        label.backgroundColor = .picoAlphaBlue
+        return label
+    }()
+    
+    private let infoButton: UIButton = {
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 25, weight: .regular)
+        let image = UIImage(systemName: "info.circle.fill", withConfiguration: imageConfig)
+        let button = UIButton()
+        button.setImage(image, for: .normal)
+        button.tintColor = .white.withAlphaComponent(0.8)
+        return button
+    }()
+    
+    // MARK: - override
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        scrollView.delegate = self
+        addSubView()
+        makeConstraints()
+        loadImages()
     }
+    
+    func addSubView() {
+        for viewItem in [scrollView, pageControl, pickBackButton, disLikeButton, likeButton, infoHStack] {
+            view.addSubview(viewItem)
+        }
+        infoHStack.addArrangedSubview(infoVStack)
+        infoHStack.addArrangedSubview(infoButton)
+        
+        infoVStack.addArrangedSubview(infoNameAgeLabel)
+        infoVStack.addArrangedSubview(infoLocationLabel)
+        infoVStack.addArrangedSubview(infoMBTILabel)
+    }
+    
+    func makeConstraints() {
+        scrollView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(10)
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-10)
+        }
+        
+        pageControl.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(10)
+        }
+        pageControl.numberOfPages = imageUrl.count
+        
+        pickBackButton.snp.makeConstraints { make in
+            make.leading.equalTo(view.safeAreaLayoutGuide).offset(paddingVertical)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-paddingBottom)
+            make.width.equalTo(buttonFrame)
+            make.height.equalTo(buttonFrame)
+        }
+        
+        disLikeButton.snp.makeConstraints { make in
+            make.trailing.equalTo(likeButton.snp.leading).offset(-paddingVertical + 10)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-paddingBottom)
+            make.width.equalTo(buttonFrame)
+            make.height.equalTo(buttonFrame)
+        }
+        
+        likeButton.snp.makeConstraints { make in
+            make.trailing.equalTo(view.safeAreaLayoutGuide).offset(-paddingVertical)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-paddingBottom)
+            make.width.equalTo(buttonFrame)
+            make.height.equalTo(buttonFrame)
+        }
+        
+        infoHStack.snp.makeConstraints { make in
+            make.leading.equalTo(view.safeAreaLayoutGuide).offset(paddingVertical)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).offset(-paddingVertical)
+            make.bottom.equalTo(disLikeButton.snp.top).offset(-paddingBottom + 20)
+            make.height.equalTo(100)
+        }
+        
+        infoButton.snp.makeConstraints { make in
+            make.trailing.equalTo(infoHStack.snp.trailing)
+            make.width.equalTo(25)
+            make.height.equalTo(25)
+        }
+        
+        infoMBTILabel.snp.makeConstraints { make in
+            make.leading.equalTo(infoHStack.snp.leading)
+            make.trailing.equalTo(infoMBTILabel.snp.leading).offset(40)
+            make.width.equalTo(40)
+        }
+    }
+    private func loadImages() {
+        for (index, urlStr) in imageUrl.enumerated() {
+            if let url = URL(string: urlStr) {
+                let imageView = UIImageView()
+                imageView.contentMode = .scaleAspectFill
+                imageView.clipsToBounds = true
+                imageView.layer.cornerRadius = 10
+                
+                scrollView.addSubview(imageView)
+                
+                imageView.snp.makeConstraints { make in
+                    make.leading.equalTo(scrollView).offset(CGFloat(index) * view.frame.width + 10)
+                    make.trailing.equalTo(scrollView).offset(-10)
+                    make.top.equalTo(scrollView)
+                    make.width.equalTo(view).offset(-20)
+                    make.height.equalTo(scrollView)
+                }
+                
+                DispatchQueue.global().async {
+                    if let data = try? Data(contentsOf: url), let image = UIImage(data: data) {
+                        DispatchQueue.main.async { [self] in
+                            imageView.image = image
+                            
+                            if index == imageUrl.count - 1 {
+                                scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(imageUrl.count), height: scrollView.frame.height)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+}
 
+extension HomeTabImageViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = Int(scrollView.contentOffset.x / scrollView.frame.size.width)
+        pageControl.currentPage = pageIndex
+    }
 }
--- Pico/Home/HomeViewController.swift
+++ Pico/Home/HomeViewController.swift
@@ -11,97 +11,23 @@ import SwiftUI
 
 final class HomeViewController: UIViewController {
     
-    private let imageUrl: [String] = [
-        "https://image5jvqbd.fmkorea.com/files/attach/new2/20211225/3655109/3113058505/4195166827/e130faca7194985e4f162b3583d52853.jpg",
-        "https://img.dmitory.com/img/202107/2lh/a8H/2lha8HnRr6Q046GGGQ0uwM.jpg",
-        "https://i.namu.wiki/i/jUHcYJjORbNSurOw8cwl-g8jduAT01mhJJkF5oDbvyae_1hkSExnUQ0I5fDKgebUKzSFjSFhRheeSI9-rfpuDU8RJ9wqo5KwIodMVjuzKT2o6RK0IutUtsKWZrYxzT-cOvxKhbPm9c3PXo5H-OvBCA.webp"
-    ]
-    //    private var imageUrl: [String]
-    //    init(imageUrl: [String]) {
-    //            self.imageUrl = imageUrl
-    //            super.init(nibName: nil, bundle: nil)
-    //        }
-    //
-    //    required init?(coder: NSCoder) {
-    //        fatalError("init(coder:) has not been implemented")
-    //    }
     private let navigationBar: UINavigationBar = {
         let navigationBar = UINavigationBar()
         return navigationBar
     }()
     
-    private let scrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.isPagingEnabled = true
-        return scrollView
-    }()
-    
-    private let pageControl: UIPageControl = {
-        let pageControl = UIPageControl()
-        pageControl.currentPageIndicatorTintColor = UIColor.black
-        pageControl.pageIndicatorTintColor = UIColor.lightGray
-        return pageControl
-    }()
-    
-    private let pickBackButton: UIButton = {
-        let imageConfig = UIImage.SymbolConfiguration(pointSize: 40, weight: .regular)
-        let image = UIImage(systemName: "arrow.uturn.backward", withConfiguration: imageConfig)
-        let button = UIButton()
-        button.setImage(image, for: .normal)
-        button.tintColor = .white
-        button.backgroundColor = .gray.withAlphaComponent(0.5)
-        button.frame = CGRect(x: 0, y: 0, width: 65, height: 65)
-        button.layer.cornerRadius = 0.5 * button.bounds.size.width
-        return button
-    }()
-    
-    private let disLikeButton: UIButton = {
-        let imageConfig = UIImage.SymbolConfiguration(pointSize: 40, weight: .regular)
-        let image = UIImage(systemName: "hand.thumbsdown.fill", withConfiguration: imageConfig)
-        let button = UIButton()
-        button.setImage(image, for: .normal)
-        button.tintColor = .white
-        button.backgroundColor = .picoBlue
-        button.frame = CGRect(x: 0, y: 0, width: 65, height: 65)
-        button.layer.cornerRadius = 0.5 * button.bounds.size.width
-        return button
-    }()
-    
-    private let likeButton: UIButton = {
-        let imageConfig = UIImage.SymbolConfiguration(pointSize: 40, weight: .regular)
-        let image = UIImage(systemName: "hand.thumbsup.fill", withConfiguration: imageConfig)
-        let button = UIButton()
-        button.setImage(image, for: .normal)
-        button.tintColor = .white
-        button.backgroundColor = .picoBlue
-        button.frame = CGRect(x: 0, y: 0, width: 65, height: 65)
-        button.layer.cornerRadius = 0.5 * button.bounds.size.width
-        return button
-    }()
-    private func createButton(imageName: String, backgroundColor: UIColor) -> UIButton {
-        let imageConfig = UIImage.SymbolConfiguration(pointSize: 40, weight: .regular)
-        let image = UIImage(systemName: imageName, withConfiguration: imageConfig)
-        let button = UIButton()
-        button.setImage(image, for: .normal)
-        button.tintColor = .white
-        button.backgroundColor = backgroundColor
-        button.frame = CGRect(x: 0, y: 0, width: 65, height: 65)
-        button.layer.cornerRadius = 0.5 * button.bounds.size.width
-        return button
-    }
     // MARK: - override
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        scrollView.delegate = self
         configLogoBarItem()
-        confignavigationBarItem()
+        configNavigationBarItem()
         addSubView()
         makeConstraints()
-        loadImages()
+        addTabImageViewController()
     }
     
-    func confignavigationBarItem() {
+    func configNavigationBarItem() {
         let notificationImage = UIImage(systemName: "bell.fill")
         let filterImage = UIImage(systemName: "slider.horizontal.3")
         
@@ -110,93 +36,22 @@ final class HomeViewController: UIViewController {
         
         notificationButton.tintColor = .darkGray
         filterButton.tintColor = .darkGray
-        navigationItem.rightBarButtonItems = [notificationButton, filterButton]
+        navigationItem.rightBarButtonItems = [filterButton, notificationButton]
     }
     
     func addSubView() {
         view.addSubview(navigationBar)
-        view.addSubview(scrollView)
-        view.addSubview(pageControl)
-        view.addSubview(pickBackButton)
-        view.addSubview(disLikeButton)
-        view.addSubview(likeButton)
         
     }
     
     func makeConstraints() {
-        scrollView.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(10)
-            make.leading.equalToSuperview()
-            make.trailing.equalToSuperview()
-            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-5)
-        }
         
-        pageControl.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(10)
-        }
-        pageControl.numberOfPages = imageUrl.count
-        
-        pickBackButton.snp.makeConstraints { make in
-            make.leading.equalTo(view.safeAreaLayoutGuide).offset(20)
-            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-30)
-            make.width.equalTo(65)
-            make.height.equalTo(65)
-        }
-        
-        disLikeButton.snp.makeConstraints { make in
-            make.trailing.equalTo(likeButton.snp.leading).offset(-20)
-            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-30)
-            make.width.equalTo(65)
-            make.height.equalTo(65)
-        }
-        
-        likeButton.snp.makeConstraints { make in
-            make.trailing.equalTo(view.safeAreaLayoutGuide).offset(-20)
-            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-30)
-            make.width.equalTo(65)
-            make.height.equalTo(65)
-        }
-        
-    }
-    private func loadImages() {
-        for (index, urlStr) in imageUrl.enumerated() {
-            if let url = URL(string: urlStr) {
-                let imageView = UIImageView()
-                imageView.contentMode = .scaleAspectFill
-                imageView.clipsToBounds = true
-                imageView.layer.cornerRadius = 10
-                
-                scrollView.addSubview(imageView)
-                
-                imageView.snp.makeConstraints { make in
-                    make.leading.equalTo(scrollView).offset(CGFloat(index) * view.frame.width + 10)
-                    make.trailing.equalTo(scrollView).offset(-10)
-                    make.top.equalTo(scrollView)
-                    make.width.equalTo(view).offset(-20)
-                    make.height.equalTo(scrollView)
-                }
-                
-                DispatchQueue.global().async {
-                    if let data = try? Data(contentsOf: url), let image = UIImage(data: data) {
-                        DispatchQueue.main.async { [self] in
-                            imageView.image = image
-                            
-                            if index == imageUrl.count - 1 {
-                                scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(imageUrl.count), height: scrollView.frame.height)
-                            }
-                        }
-                    }
-                }
-            }
-        }
     }
     
-}
-
-extension HomeViewController: UIScrollViewDelegate {
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let pageIndex = Int(scrollView.contentOffset.x / scrollView.frame.size.width)
-        pageControl.currentPage = pageIndex
+    func addTabImageViewController() {
+        let tabImageViewController = HomeTabImageViewController()
+        addChild(tabImageViewController)
+        view.addSubview(tabImageViewController.view)
+        tabImageViewController.didMove(toParent: self)
     }
 }
