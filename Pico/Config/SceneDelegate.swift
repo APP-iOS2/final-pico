a56365494f0f07dbc60a6859af0025da5ad1a97d
Jehyeon Lee <dasom8899981@gmail.com>
2023-09-25 17:17:51 +0900

    feat: 로그인 시나리오대로 뷰 작성
    LoginSuccessViewController: 피그마 대로 디자인 완성
    SignInViewController: 번호대로 기입시 텍스트 색상변경 및 다음화면으로 넘어갈 수 있게 함

--- Pico/Config/SceneDelegate.swift
+++ Pico/Config/SceneDelegate.swift
@@ -15,7 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
                 
-        let tabBarController = TabBarController()
+//        let tabBarController = TabBarController()
+        let tabBarController = UINavigationController(rootViewController: SignViewController())
         window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
     }
