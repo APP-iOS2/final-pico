dda66deddf109891366257baa7e7ab76a2acfcef
Jehyeon Lee <dasom8899981@gmail.com>
2023-09-25 17:17:51 +0900

    feat: 로그인 시나리오대로 뷰 작성
    LoginSuccessViewController: 피그마 대로 디자인 완성
    SignInViewController: 번호대로 기입시 텍스트 색상변경 및 다음화면으로 넘어갈 수 있게 함

---
+++ Pico/Sign/SignIn/LoginSuccessViewController.swift
@@ -0,0 +1,75 @@
+//
+//  LoginSuccessViewController.swift
+//  Pico
+//
+//  Created by LJh on 2023/09/25.
+//
+
+import UIKit
+
+class LoginSuccessViewController: UIViewController {
+
+    private let notifyLabel: UILabel = {
+        let label = UILabel()
+        label.text = "로그인완료"
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.font = UIFont.picoTitleFont
+        return label
+    }()
+    
+    private let checkImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(systemName: "checkmark.circle")
+        return imageView
+    }()
+    
+    private let nextButton: UIButton = {
+        let button = CommonButton(type: .custom)
+        button.setTitle("완료", for: .normal)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        addSubViews()
+        makeConstraints()
+    }
+    
+    private func addSubViews() {
+        view.addSubview(notifyLabel)
+        view.addSubview(checkImageView)
+        view.addSubview(nextButton)
+        
+    }
+    
+    private func makeConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        notifyLabel.snp.makeConstraints { make in
+            make.top.equalTo(safeArea).offset(10)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+        }
+        
+        checkImageView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(150)
+            make.trailing.equalToSuperview().offset(-150)
+            
+            make.centerY.equalTo(safeArea)
+            
+        }
+        
+        nextButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.height.equalTo(50)
+            make.leading.equalTo(20)
+            make.trailing.equalTo(-20)
+            make.bottom.equalTo(safeArea).offset(-30)
+        }
+
+    }
+
+}
--- Pico/Sign/SignIn/SignInViewController.swift
+++ Pico/Sign/SignIn/SignInViewController.swift
@@ -35,7 +35,6 @@ final class SignInViewController: UIViewController {
         button.imageView?.contentMode = .scaleAspectFit
         button.setTitleColor(.white, for: .normal)
         button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
-        //        button.addTarget(self, action: #selector(tappedSignInButton), for: .touchUpInside)
         return button
     }()
     
@@ -50,21 +49,65 @@ final class SignInViewController: UIViewController {
         return stackView
     }()
     
+    private let nextButton: UIButton = {
+        let button = CommonButton(type: .custom)
+        button.setTitle("다음", for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         addSubViews()
         makeConstraints()
         configTextfield()
+        configButton()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardUp), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDown), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     private func configTextfield() {
         phoneNumberTextField.delegate = self
     }
     
+    private func configButton() {
+        phoneNumberCancleButton.addTarget(self, action: #selector(tappedSignInButton), for: .touchUpInside)
+    }
+    
+    @objc private func tappedSignInButton() {
+        print(#function)
+        phoneNumberTextField.text = ""
+    }
+    
+    @objc private func tappedNextButton() {
+        let viewController = LoginSuccessViewController()
+        self.navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    @objc private func keyboardUp(notification: NSNotification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+           let keyboardRectangle = keyboardFrame.cgRectValue
+       
+            UIView.animate(
+                withDuration: 0.5
+                , animations: {
+                    self.nextButton.transform = CGAffineTransform(translationX: 0, y: -keyboardRectangle.height)
+                }
+            )
+        }
+    }
+    
+    @objc private func keyboardDown() {
+        self.nextButton.transform = .identity
+    }
+    
     private func addSubViews() {
         view.addSubview(notifyLabel)
         view.addSubview(stackView)
+        view.addSubview(nextButton)
     }
     
     private func makeConstraints() {
@@ -79,10 +122,17 @@ final class SignInViewController: UIViewController {
         stackView.snp.makeConstraints { make in
             make.top.equalTo(notifyLabel.snp.bottom).offset(20)
             make.leading.equalToSuperview().offset(20)
-            make.trailing.equalToSuperview().offset(-20)
+            make.trailing.equalToSuperview().offset(-60)
             make.height.equalTo(50)
         }
         
+        nextButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.height.equalTo(50)
+            make.leading.equalTo(20)
+            make.trailing.equalTo(-20)
+            make.bottom.equalTo(safeArea).offset(-30)
+        }
     }
 }
 
@@ -92,12 +142,16 @@ extension SignInViewController: UITextFieldDelegate {
         self.view.endEditing(true)
     }
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        if Int(string) != nil || string == "" {
+        if Int(string) != nil || string.isEmpty {
             if (textField.text?.count)! > 10 {
-                phoneNumberTextField.textColor = .black
                 return false
-            } else {
+            } else if (textField.text?.count)! > 9 {
+                phoneNumberTextField.textColor = .picoBlue
+                nextButton.addTarget(self, action: #selector(tappedNextButton), for: .touchUpInside)
                 
+                return true
+            } else {
+                phoneNumberTextField.textColor = .gray
                 return true
             }
         }
