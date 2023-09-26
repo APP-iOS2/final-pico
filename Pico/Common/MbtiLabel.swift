0a1cab29ba4793154a50911b57e50412a6b19db1
HANLeeeee <hanleeeee@naver.com>
2023-09-26 10:15:50 +0900

    feat: label 추가

---
+++ Pico/Common/MbtiLabel.swift
@@ -0,0 +1,19 @@
+//
+//  MbtiLabel.swift
+//  Pico
+//
+//  Created by 최하늘 on 2023/09/26.
+//
+
+import UIKit
+
+final class MbtiLabel: UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
