8fe9d34aa8052a68b8a2ec8b6bf25010abf0516a
Tori <ttang1135@naver.com>
2023-09-26 10:38:30 +0900

    feat: TabBar 화면 전환 애니메이션 & 탭바 레이블링

--- Pico/TabBar/TabBarController.swift
+++ Pico/TabBar/TabBarController.swift
@@ -23,11 +23,11 @@ final class TabBarController: UITabBarController {
         let entViewController = UINavigationController(rootViewController: EntViewController())
         let mypageViewController = UINavigationController(rootViewController: MypageViewController())
         
-        homeViewController.tabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "house.fill"), tag: 0)
-        mailViewController.tabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "envelope.fill"), tag: 1)
-        likeViewController.tabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "heart.fill"), tag: 2)
-        entViewController.tabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "gamecontroller.fill"), tag: 3)
-        mypageViewController.tabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "person.fill"), tag: 4)
+        homeViewController.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house.fill"), tag: 0)
+        mailViewController.tabBarItem = UITabBarItem(title: "Mail", image: UIImage(systemName: "envelope.fill"), tag: 1)
+        likeViewController.tabBarItem = UITabBarItem(title: "Like", image: UIImage(systemName: "heart.fill"), tag: 2)
+        entViewController.tabBarItem = UITabBarItem(title: "Ent", image: UIImage(systemName: "gamecontroller.fill"), tag: 3)
+        mypageViewController.tabBarItem = UITabBarItem(title: "MyPage", image: UIImage(systemName: "person.fill"), tag: 4)
         
         homeViewController.navigationBar.prefersLargeTitles = false
         mailViewController.navigationBar.prefersLargeTitles = false
@@ -36,5 +36,13 @@ final class TabBarController: UITabBarController {
         mypageViewController.navigationBar.prefersLargeTitles = false
         
         self.viewControllers = [homeViewController, mailViewController, likeViewController, entViewController, mypageViewController]
+        delegate = self
+    }
+}
+
+extension TabBarController: UITabBarControllerDelegate {
+    
+    func tabBarController(_ tabBarController: UITabBarController, animationControllerForTransitionFrom fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return MyTransition(viewControllers: tabBarController.viewControllers)
     }
 }
