89133c407f687843091239e0b2fe2091bfefed43
minki-kim-git <rlaalsrl1227@gmail.com>
2023-09-26 14:44:15 +0900

    feat: mypage에 tableview 추가

---
+++ Pico/Mypage/SubComponents/TableView/Cell/MyPageCollectionCell.swift
@@ -0,0 +1,69 @@
+//
+//  MyPageCollectionCell.swift
+//  Pico
+//
+//  Created by 김민기 on 2023/09/26.
+//
+
+import UIKit
+import SnapKit
+
+final class MyPageCollectionCell: UICollectionViewCell {
+    
+    private let image: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .picoFontBlack
+        label.font = .picoSubTitleFont
+        return label
+    }()
+    private let subTitleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .picoFontGray
+        label.font = .picoDescriptionFont
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubView()
+        makeConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func configure(imageName: String, title: String, subTitle: String) {
+        image.image = UIImage(named: imageName)
+        titleLabel.text = title
+        subTitleLabel.text = subTitle
+    }
+    
+    private func addSubView() {
+        [image, titleLabel, subTitleLabel].forEach {
+            contentView.addSubview($0)
+        }
+    }
+    
+    private func makeConstraints() {
+        image.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(3)
+            make.centerX.equalToSuperview()
+            make.height.width.equalTo(55)
+//            make.width.equalTo(40)
+        }
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(image.snp.bottom).offset(-6)
+            make.centerX.equalToSuperview()
+        }
+        subTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom)
+            make.centerX.equalToSuperview()
+        }
+    }
+}
