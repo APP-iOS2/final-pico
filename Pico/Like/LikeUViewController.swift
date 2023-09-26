69210152ac5e5d3d6a30017147c4b634ecf928c9
Tori <ttang1135@naver.com>
2023-09-26 15:47:19 +0900

    feat: 좋아요 뷰 컬렉션 뷰 디자인

--- Pico/Like/LikeUViewController.swift
+++ Pico/Like/LikeUViewController.swift
@@ -8,13 +8,22 @@
 import UIKit
 
 final class LikeUViewController: UIViewController {
-    let emptyView: LikeEmptyView = LikeEmptyView(frame: CGRect(x: 0, y: 0, width: Screen.height, height: Screen.width), type: .iLikeU)
+    private let emptyView: LikeEmptyView = LikeEmptyView(frame: CGRect(x: 0, y: 0, width: Screen.height, height: Screen.width), type: .iLikeU)
+    private let collectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    private let imageUrls: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         addViews()
         makeConstraints()
         configButtons()
+        configCollectionView()
+    }
+    
+    private func configCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(LikeCollectionViewCell.self, forCellWithReuseIdentifier: LikeCollectionViewCell.identifier)
     }
     
     private func configButtons() {
@@ -22,12 +31,23 @@ final class LikeUViewController: UIViewController {
     }
     
     private func addViews() {
-        view.addSubview(emptyView)
+        if imageUrls.isEmpty {
+            view.addSubview(emptyView)
+        } else {
+            view.addSubview(collectionView)
+        }
     }
     
     private func makeConstraints() {
-        emptyView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+        if imageUrls.isEmpty {
+            emptyView.snp.makeConstraints { make in
+                make.edges.equalToSuperview()
+            }
+        } else {
+            collectionView.snp.makeConstraints { make in
+                make.top.leading.equalToSuperview().offset(10)
+                make.trailing.bottom.equalToSuperview().offset(-10)
+            }
         }
     }
     
@@ -35,5 +55,35 @@ final class LikeUViewController: UIViewController {
         if let tabBarController = self.tabBarController as? TabBarController {
             tabBarController.selectedIndex = 0
         }
+    }
+}
+
+extension LikeUViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageUrls.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LikeCollectionViewCell.identifier, for: indexPath) as? LikeCollectionViewCell else { return UICollectionViewCell() }
+        loadAsyncImage(imageView: cell.userImageView, urlString: imageUrls[indexPath.row])
+        cell.deleteButton.isHidden = true
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = view.frame.width / 2 - 20
+        return CGSize(width: width, height: width * 1.5)
+    }
+    
+    private func loadAsyncImage(imageView: UIImageView, urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data,
+                  response != nil,
+                  error == nil else { return }
+            DispatchQueue.main.async {
+                imageView.image = UIImage(data: data) ?? UIImage()
+            }
+        }.resume()
     }
 }
