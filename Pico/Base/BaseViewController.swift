cf743abed9a61804fc8c3942b0aec517b77ed410
HANLeeeee <hanleeeee@naver.com>
2023-09-27 14:32:22 +0900

    feat: BaseViewController 생성

--- Pico/Base/BaseViewController.swift
+++ Pico/Base/BaseViewController.swift
@@ -6,39 +6,22 @@
 //
 
 import UIKit
-import SnapKit
+
+/*
+ 사용법: 상속해서 사용하시면 됩니다
+ class MainViewController: BaseViewController {
+*/
 
 class BaseViewController: UIViewController {
     
-    private let labelView: CommonTextField = CommonTextField(maxLength: 10)
-    private let labelView1: MBTILabelView = MBTILabelView()
-    private let labelView2: MBTILabelView = MBTILabelView()
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        configUI()
+    }
+    
+    private func configUI() {
         view.backgroundColor = .systemBackground
-        view.addSubview(labelView)
-        view.addSubview(labelView1)
-        view.addSubview(labelView2)
-
-        labelView.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(200)
-            make.leading.trailing.equalToSuperview().inset(100)
-            make.height.equalTo(40)
-        }
-        
-        labelView1.snp.makeConstraints { make in
-            make.top.equalTo(labelView.snp.bottom).offset(40)
-            make.leading.equalTo(10)
-            make.height.equalTo(labelView1.frame.size.height)
-            make.width.equalTo(labelView1.frame.size.width)
-        }
-        
-        labelView2.snp.makeConstraints { make in
-            make.top.equalTo(labelView1.snp.top)
-            make.trailing.equalToSuperview().offset(40)
-            make.height.equalTo(labelView2.frame.size.height)
-            make.width.equalTo(labelView2.frame.size.width)
-        }
+        configBackButton()
+        configLogoBarItem()
     }
 }
