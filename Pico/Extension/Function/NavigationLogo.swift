314988d85d657939f36b28bd7141ab8c8555299f
HANLeeeee <hanleeeee@naver.com>
2023-09-27 10:54:58 +0900

    feat: navigation logo 레이아웃 수정

--- Pico/Extension/Function/NavigationLogo.swift
+++ Pico/Extension/Function/NavigationLogo.swift
@@ -6,16 +6,34 @@
 //
 
 import UIKit
+import SnapKit
 
 extension UIViewController {
     /// logo leftBarButton
     /// -> viewDidLoad에서 호출
     func configLogoBarItem() {
-        var logoImage = UIImage(named: "logo")
-        logoImage = logoImage?.withRenderingMode(.alwaysOriginal)
+        let logoButton = UIButton(type: .system)
+        let image = UIImage(named: "logo")
+        logoButton.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+        let leftItem = UIBarButtonItem(customView: logoButton)
+        leftItem.customView?.snp.makeConstraints({ make in
+            make.height.equalTo(28)
+            make.width.equalTo(getRatio(image, height: 28))
+        })
+        navigationItem.leftBarButtonItem = leftItem
+    }
+    
+    private func getRatio(_ image: UIImage?, height: CGFloat = 0, width: CGFloat = 0) -> CGFloat {
+        guard let image = image else { return  0 }
+        let widthRatio = CGFloat(image.size.width / image.size.height)
+        let heightRatio = CGFloat(image.size.height / image.size.width)
         
-        let logoButton = UIBarButtonItem(image: logoImage, style: .plain, target: nil, action: nil)
-        logoButton.isEnabled = false
-        navigationItem.leftBarButtonItem = logoButton
+        if height != 0 {
+            return height / heightRatio
+        }
+        if width != 0 {
+            return width / widthRatio
+        }
+        return 0
     }
 }
