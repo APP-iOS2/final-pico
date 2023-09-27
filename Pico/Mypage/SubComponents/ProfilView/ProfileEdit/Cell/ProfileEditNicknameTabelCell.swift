10f4ea916b55fd9ea78976f60782263b1c977877
minki-kim-git <rlaalsrl1227@gmail.com>
2023-09-27 14:02:08 +0900

    feat: 프로필 수정뷰 테이블뷰, 셀추가

---
+++ Pico/Mypage/SubComponents/ProfilView/ProfileEdit/Cell/ProfileEditNicknameTabelCell.swift
@@ -0,0 +1,68 @@
+//
+//  ProfileEditNicknameTabelCell.swift
+//  Pico
+//
+//  Created by 김민기 on 2023/09/26.
+//
+
+import UIKit
+
+final class ProfileEditNicknameTabelCell: UITableViewCell {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .picoFontBlack
+        label.font = UIFont.picoSubTitleFont
+        label.text = "닉네임 변경"
+        return label
+    }()
+    private lazy var nicknameChangeButton: UIButton = {
+        let button = UIButton()
+        let image = UIImage(named: "chu")?.resized(toSize: CGSize(width: 30, height: 30))
+        var configuration = UIButton.Configuration.plain()
+        configuration.image = image
+        configuration.imagePlacement = .leading
+        configuration.imagePadding = 3
+        configuration.subtitle = "50"
+        button.configuration = configuration
+        button.layer.masksToBounds = false
+        button.layer.cornerRadius = 15
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.picoBlue.cgColor
+        button.addTarget(self, action: #selector(tappedbutton), for: .touchUpInside)
+        return button
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubView()
+        makeConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc private func tappedbutton() {
+        print("tap")
+    }
+    
+    private func addSubView() {
+        [titleLabel, nicknameChangeButton].forEach {
+            contentView.addSubview($0)
+        }
+    }
+    
+    private func makeConstraints() {
+        titleLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().offset(15)
+        }
+        nicknameChangeButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().offset(-15)
+            make.height.equalTo(30)
+            make.width.equalTo(100)
+        }
+    }
+}
---
+++ Pico/Mypage/SubComponents/ProfilView/ProfileEdit/ProfileEditViewController.swift
@@ -0,0 +1,115 @@
+//
+//  ProfileEditViewController.swift
+//  Pico
+//
+//  Created by 김민기 on 2023/09/27.
+//
+
+import UIKit
+
+final class ProfileEditViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let view = UITableView(frame: .zero, style: .plain)
+        view.register(ProfileEditImageTableCell.self, forCellReuseIdentifier: Identifier.TableCell.profileEditImageTableCell)
+        view.register(ProfileEditNicknameTabelCell.self, forCellReuseIdentifier: Identifier.TableCell.profileEditNicknameTabelCell)
+        view.backgroundColor = .systemBackground
+        view.separatorStyle = .none
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configNavigation()
+        configTableView()
+        addViews()
+        makeConstraints()
+    }
+    
+    private func configNavigation() {
+        title = "프로필 수정"
+    }
+    
+    private func configTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    private func addViews() {
+        [tableView].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    private func makeConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
+extension ProfileEditViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0...1:
+            return 1
+        default:
+            return 0
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.section {
+        case 0:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: Identifier.TableCell.profileEditImageTableCell, for: indexPath) as? ProfileEditImageTableCell else { return UITableViewCell() }
+            
+            return cell
+        case 1:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: Identifier.TableCell.profileEditNicknameTabelCell, for: indexPath) as? ProfileEditNicknameTabelCell else { return UITableViewCell() }
+            cell.selectionStyle = .none
+            return cell
+        default:
+            return UITableViewCell()
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 0:
+            return 155
+        case 1...3:
+            return 45
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        switch section {
+        case 0:
+            let view = UIView()
+            return view
+        default:
+            return nil
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        switch section {
+        case 0:
+            return 20.0
+        default:
+            return 10
+        }
+            
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
