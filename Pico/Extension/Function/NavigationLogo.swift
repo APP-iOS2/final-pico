b3d016c9f929d6840e9d411a51849431f6d07a15
HANLeeeee <hanleeeee@naver.com>
2023-09-27 14:14:23 +0900

    feat: 네비게이션 백버튼 커스텀 생성

--- Pico/Extension/Function/NavigationLogo.swift
+++ Pico/Extension/Function/NavigationLogo.swift
@@ -15,15 +15,17 @@ extension UIViewController {
         let logoButton = UIButton(type: .system)
         let image = UIImage(named: "logo")
         logoButton.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+        
         let leftItem = UIBarButtonItem(customView: logoButton)
         leftItem.customView?.snp.makeConstraints({ make in
             make.height.equalTo(28)
             make.width.equalTo(getRatio(image, height: 28))
         })
+        leftItem.isEnabled = false
         navigationItem.leftBarButtonItem = leftItem
     }
     
-    private func getRatio(_ image: UIImage?, height: CGFloat = 0, width: CGFloat = 0) -> CGFloat {
+    func getRatio(_ image: UIImage?, height: CGFloat = 0, width: CGFloat = 0) -> CGFloat {
         guard let image = image else { return  0 }
         let widthRatio = CGFloat(image.size.width / image.size.height)
         let heightRatio = CGFloat(image.size.height / image.size.width)
