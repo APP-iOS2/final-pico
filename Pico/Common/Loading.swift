e71ced5c3ed4f3354ccc9dbea023f65b6e446b28
Tori <ttang1135@naver.com>
2023-10-04 03:24:49 +0900

    feat: LoadingView

---
+++ Pico/Common/Loading.swift
@@ -0,0 +1,38 @@
+//
+//  Loading.swift
+//  Pico
+//
+//  Created by 방유빈 on 2023/10/04.
+//
+
+import UIKit
+
+class Loading {
+    static func showLoading() {
+        DispatchQueue.main.async {
+            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+               let window = windowScene.windows.last {
+                let loadingView: LoadingAnimationView
+                if let existedView = window.subviews.first(where: {
+                    $0 is LoadingAnimationView
+                }) as? LoadingAnimationView {
+                    loadingView = existedView
+                } else {
+                    loadingView = LoadingAnimationView()
+                    loadingView.frame = window.frame
+                    window.addSubview(loadingView)
+                }
+                loadingView.animate()
+            }
+        }
+    }
+
+    static func hideLoading() {
+        DispatchQueue.main.async {
+            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+               let window = windowScene.windows.last {
+                window.subviews.filter({ $0 is LoadingAnimationView }).forEach { $0.removeFromSuperview() }
+            }
+        }
+    }
+}
