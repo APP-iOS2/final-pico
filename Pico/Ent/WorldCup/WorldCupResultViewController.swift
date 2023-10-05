1e974d17ef38d7787f9f05af86477801248aa4f7
Youngs5 <oys9330@gmail.com>
2023-10-05 14:37:42 +0900

    Refactor: chatButton, cancelButton Rx로 변경

--- Pico/Ent/WorldCup/WorldCupResultViewController.swift
+++ Pico/Ent/WorldCup/WorldCupResultViewController.swift
@@ -7,10 +7,13 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 final class WorldCupResultViewController: UIViewController {
     
     var selectedItem: User?
+    private let disposeBag = DisposeBag()
     
     private let backgroundImageView: UIImageView = {
         let imageView = UIImageView(image: UIImage(named: "gameBackground"))
@@ -52,7 +55,6 @@ final class WorldCupResultViewController: UIViewController {
     private lazy var chatButton: CommonButton = {
         let button = CommonButton()
         button.setTitle("채팅 신청하기", for: .normal)
-        button.addTarget(self, action: #selector(tappedChatButton), for: .touchUpInside)
         return button
     }()
     
@@ -70,7 +72,6 @@ final class WorldCupResultViewController: UIViewController {
         button.setTitle("채팅 신청하지 않고 나가기", for: .normal)
         button.setTitleColor(.picoAlphaBlue, for: .normal)
         button.titleLabel?.font = UIFont.picoButtonFont
-        button.addTarget(self, action: #selector(tappedCancelButton), for: .touchUpInside)
         return button
     }()
     
@@ -81,6 +82,7 @@ final class WorldCupResultViewController: UIViewController {
         makeConstraints()
         configResultUserCell()
         addShadow()
+        setupRxBindings()
         hideBackButton()
     }
     
@@ -163,17 +165,50 @@ final class WorldCupResultViewController: UIViewController {
         resultUserView.layer.shadowRadius = radius
     }
     
-    @objc func tappedChatButton() {
-        // 채팅 신청하는 것으로 바뀌어야 함
-        if let navigationController = self.navigationController {
-            navigationController.popToRootViewController(animated: true)
-        }
+    private func setupRxBindings() {
+        chatButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                guard let self = self else { return }
+                self.tappedChatButton()
+            })
+            .disposed(by: disposeBag)
+        
+        cancelButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                guard let self = self else { return }
+                self.tappedCancelButton()
+            })
+            .disposed(by: disposeBag)
     }
     
-    @objc func tappedCancelButton() {
-        if let navigationController = self.navigationController {
-            navigationController.popToRootViewController(animated: true)
+    private func tappedChatButton() {
+        let alertController = UIAlertController(title: "채팅 신청 고?", message: "오늘 결혼쌉가능", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        let confirmAction = UIAlertAction(title: "바로 고", style: .default) { [weak self] _ in
+            if let navigationController = self?.navigationController {
+                navigationController.popToRootViewController(animated: true)
+            } else {
+                self?.dismiss(animated: true, completion: nil)
+            }
         }
+        alertController.addAction(cancelAction)
+        alertController.addAction(confirmAction)
+        self.present(alertController, animated: true, completion: nil)
     }
-
+    
+    private func tappedCancelButton() {
+        let alertController = UIAlertController(title: "정말로 나가실건가용~?", message: "지금 채팅 신청하면 50% 할인해드리는뎅ㅋ", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "나가기", style: .cancel, handler: nil)
+        let confirmAction = UIAlertAction(title: "고민해볼래", style: .default) { [weak self] _ in
+            if let navigationController = self?.navigationController {
+                navigationController.popToRootViewController(animated: true)
+            } else {
+                self?.dismiss(animated: true, completion: nil)
+            }
+        }
+        alertController.addAction(cancelAction)
+        alertController.addAction(confirmAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
 }
