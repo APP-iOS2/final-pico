875e61dc8a17978efa99240ee9f3974803eb9a6b
Youngs5 <oys9330@gmail.com>
2023-10-05 15:34:21 +0900

    Refactor: 게임시작버튼 Rx 변경

--- Pico/Ent/WorldCup/WorldCupViewController.swift
+++ Pico/Ent/WorldCup/WorldCupViewController.swift
@@ -7,15 +7,18 @@
 import UIKit
 import SnapKit
 import RxSwift
+import RxCocoa
 
 final class WorldCupViewController: UIViewController {
-
+    
+    private let disposeBag = DisposeBag()
+    
     private let backgroundImageView: UIImageView = {
         let imageView = UIImageView(image: UIImage(named: "gameBackground"))
         imageView.contentMode = .scaleAspectFill
         return imageView
     }()
-
+    
     private let worldCupTitleLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .center
@@ -23,7 +26,7 @@ final class WorldCupViewController: UIViewController {
         label.text = "당신의 친구에게 투표하세요"
         return label
     }()
-
+    
     private let pickMeLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .center
@@ -32,7 +35,7 @@ final class WorldCupViewController: UIViewController {
         label.textColor = .picoBlue
         return label
     }()
-
+    
     private let contentLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .center
@@ -41,14 +44,13 @@ final class WorldCupViewController: UIViewController {
         label.numberOfLines = 0
         return label
     }()
-
+    
     private lazy var gameStartButton: CommonButton = {
         let button = CommonButton()
         button.setTitle("시작", for: .normal)
-        button.addTarget(self, action: #selector(tappedGameStartButton), for: .touchUpInside)
         return button
     }()
-
+    
     private let guideLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .center
@@ -57,44 +59,45 @@ final class WorldCupViewController: UIViewController {
         label.textColor = .gray
         return label
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         addViews()
         makeConstraints()
+        configRxBinding()
         configBackButton()
     }
-
+    
     private func addViews() {
         [backgroundImageView, worldCupTitleLabel, pickMeLabel, contentLabel, gameStartButton, guideLabel].forEach { item in
             view.addSubview(item)
         }
     }
-
+    
     private func makeConstraints() {
         let padding: CGFloat = 20
         let half: CGFloat = 0.5
-
+        
         backgroundImageView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
-
+        
         worldCupTitleLabel.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(Screen.height / 5)
             make.centerX.equalToSuperview().offset(half)
         }
-
+        
         pickMeLabel.snp.makeConstraints { make in
             make.top.equalTo(worldCupTitleLabel.snp.bottom).offset(padding * half)
             make.centerX.equalToSuperview().offset(half)
         }
-
+        
         contentLabel.snp.makeConstraints { make in
             make.top.equalTo(pickMeLabel.snp.bottom).offset(padding)
             make.centerX.equalToSuperview().offset(half)
         }
-
+        
         gameStartButton.snp.makeConstraints { make in
             make.top.equalTo(contentLabel.snp.bottom).offset(Screen.height / 10)
             make.centerX.equalToSuperview().offset(half)
@@ -102,16 +105,27 @@ final class WorldCupViewController: UIViewController {
             make.trailing.equalToSuperview().offset(-Screen.width / 4)
             make.height.equalTo(Screen.width / 10)
         }
-
+        
         guideLabel.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-padding * 1.5)
         }
     }
-
-    @objc func tappedGameStartButton() {
-        let worldCupGameViewController = WorldCupGameViewController()
-        self.navigationController?.pushViewController(worldCupGameViewController, animated: true)
-        self.tabBarController?.tabBar.isHidden = true
+    private func configRxBinding() {
+        gameStartButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                guard let self = self else { return }
+                self.tappedGameStartButton()
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func tappedGameStartButton() {
+        if let navigationController = self.navigationController {
+            navigationController.pushViewController(WorldCupGameViewController(), animated: true)
+            self.tabBarController?.tabBar.isHidden = true
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
     }
 }
