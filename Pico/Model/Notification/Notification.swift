3c2ad075f8dd0ef4e078e5f0ca8c6a005d131a42
Tori <ttang1135@naver.com>
2023-10-06 17:40:51 +0900

    feat: noti 구조체 수정, loadDocumentRx 추가

--- Pico/Model/Notification/Notification.swift
+++ Pico/Model/Notification/Notification.swift
@@ -7,20 +7,32 @@
 
 import Foundation
 
-struct Notification: Codable {
-    let userId: String
-    let isNotification: Bool
-    let notiInfos: [NotiInfo]
-    
-    struct NotiInfo: Codable {
-        var id: String = UUID().uuidString
-        let notiType: NotiType
-        let sendUserId: String
-        let notiDate: Double
-    }
-}
+//struct Notification: Codable {
+//    let userId: String
+//    let isNotification: Bool
+//    let notiInfos: [NotiInfo]
+//    
+//    struct NotiInfo: Codable {
+//        var id: String = UUID().uuidString
+//        let notiType: NotiType
+//        let sendUserId: String
+//        let notiDate: Double
+//    }
+//}
 
 enum NotiType: String, Codable {
     case like
     case message
+}
+
+struct Noti {
+    let receiveId: String // 알림 받는 사람 id
+    let name: String // 보내는사람 이름
+    let age: Int //보내는사람 나이
+    let imageUrl: String //보내는 사람 첫번째 이미지
+    let notiType: NotiType
+    
+    var title: String {
+        return "\(name), \(age)"
+    }
 }
