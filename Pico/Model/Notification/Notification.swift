7e81da1779a7849104a7eceaa11a6ac56e7485f0
Tori <ttang1135@naver.com>
2023-10-06 18:58:13 +0900

    feat: noti 구조체 재수정

--- Pico/Model/Notification/Notification.swift
+++ Pico/Model/Notification/Notification.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-//struct Notification: Codable {
+// struct Notification: Codable {
 //    let userId: String
 //    let isNotification: Bool
 //    let notiInfos: [NotiInfo]
@@ -18,21 +18,25 @@ import Foundation
 //        let sendUserId: String
 //        let notiDate: Double
 //    }
-//}
+// }
 
 enum NotiType: String, Codable {
     case like
     case message
 }
 
-struct Noti {
+struct Noti: Codable {
     let receiveId: String // 알림 받는 사람 id
     let name: String // 보내는사람 이름
-    let age: Int //보내는사람 나이
-    let imageUrl: String //보내는 사람 첫번째 이미지
+    let birth: String // 보내는사람 생년월일
+    let imageUrl: String // 보내는 사람 첫번째 이미지
     let notiType: NotiType
     
-    var title: String {
-        return "\(name), \(age)"
+    var age: Int {
+        let calendar = Calendar.current
+        let currentDate = Date()
+        let birthdate = birth.toDate()
+        let ageComponents = calendar.dateComponents([.year], from: birthdate, to: currentDate)
+        return ageComponents.year ?? 0
     }
 }
