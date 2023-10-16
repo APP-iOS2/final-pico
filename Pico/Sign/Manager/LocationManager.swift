261670f12fc36d138ff49980afc7c10c41d96a6c
Jehyeon Lee <dasom8899981@gmail.com>
2023-10-16 10:05:06 +0900

    feat: 위치 매니저 추가

--- Pico/Sign/Manager/LocationManager.swift
+++ Pico/Sign/Manager/LocationManager.swift
@@ -5,10 +5,55 @@
 //  Created by LJh on 10/6/23.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import CoreLocation
+import RxSwift
+import RxRelay
 
-final class LocationManager {
+final class LocationManager: CLLocationManager, CLLocationManagerDelegate {
+    
+    var viewModel: SignUpViewModel = .shared
     static var shared = LocationManager()
+    var locationManager: CLLocationManager = CLLocationManager()
+    var currentLocation: CLLocationCoordinate2D?
     var longitude: Double?
     var latitude: Double?
+    
+    func configLocation() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        DispatchQueue.global().async {
+            if CLLocationManager.locationServicesEnabled() {
+                self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+                self.locationManager.startUpdatingLocation()
+            } else {
+                print("이미 했잖아")
+            }
+        }
+    }
+    
+    func getAddress(latitude: CLLocationDegrees?, longitude: CLLocationDegrees?, completion: @escaping (Location?) -> Void) {
+        let location = CLLocation(latitude: latitude ?? 0, longitude: longitude ?? 0)
+        CLGeocoder().reverseGeocodeLocation(location) { (placemarks, error) in
+            if let error = error {
+                print("Geocoding Error: \(error)")
+                completion(nil)
+                return
+            }
+
+            if let placemark = placemarks?.first {
+                let addressString = "\(placemark.thoroughfare ?? "") \(placemark.subThoroughfare ?? ""), \(placemark.locality ?? "") \(placemark.postalCode ?? ""), \(placemark.country ?? "")"
+                let location = Location(address: addressString, latitude: latitude ?? 0, longitude: longitude ?? 0)
+                completion(location)
+            } else {
+                completion(nil)
+            }
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
 }
