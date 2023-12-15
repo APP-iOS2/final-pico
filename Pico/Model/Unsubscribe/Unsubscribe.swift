b95f521a39bc4f1de3d6a02af5a14bdd38e8d593
HANLeeeee <hanleeeee@naver.com>
2023-12-15 10:25:10 +0900

    feat: 정지회원, 탈퇴회원 모델 추가

---
+++ Pico/Model/Unsubscribe/Unsubscribe.swift
@@ -0,0 +1,15 @@
+//
+//  Unsubscribe.swift
+//  Pico.admin
+//
+//  Created by 최하늘 on 12/15/23.
+//
+
+import Foundation
+
+/// 탈퇴
+struct Unsubscribe: Codable {
+    /// 탈퇴된 날짜
+    let createdDate: Double
+    let user: User
+}
